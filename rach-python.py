b16286a889250bcf062e5602a76b58eaf878aadc
RachealC <rcheptegei@gmail.com>
2022-08-20 12:58:03 +0300

    addition

--- rach-python.py
+++ rach-python.py
@@ -1,3 +1,25 @@
 a=2
 b=3
 print(a+b)
+# Variable
+My_name="Racheal"
+#Data
+#String
+All_char="A B C D....."
+#Floating
+
+
+#Boolean True/False
+Is_raining=True
+#None
+Lunch=None
+
+My_name="Racheal"
+My_age=12
+My_weight=12.5
+#My name is Racheal I am 12 years
+print("My name is" + My_name + "Iam" + str(My_age) + "years old")
+#print(“A simple addition calculator for user in put”)
+num1=int(input())
+num2= int(input())
+print(num1+num2)
